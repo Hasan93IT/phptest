9bdf094183cc601d28a77343cc968a7a2267d0c2
Hasan Abdullah <89301568+Hasan93IT@users.noreply.github.com>
2021-08-31 15:17:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,19 +4,19 @@
 // Zählung for-schleife
 for ($i = 1; $i <= 100; $i++) {
     // Bedingung für noramle Zählung
-    if ($i % 3 != 0 && $i % 5 != 0) {
+    if ($i % 3 !== 0 && $i % 5 !== 0) {
         echo "$i <br/>";
     }
     //vielfach von 3 und nicht vielfach von 5
-    if ($i % 3 == 0 && $i % 5 != 0) {
+    if ($i % 3 === 0 && $i % 5 !== 0) {
         echo "Fizz <br/>";
     }
     //vielfach von 5 und nicht vielfach von 3 
-    if ($i % 5 == 0 && $i % 3 != 0) {
+    if ($i % 5 === 0 && $i % 3 !== 0) {
         echo "Buzz <br/>";
     }
     //vielfach von 3 und 5
-    if ($i % 3 == 0 && $i % 5 == 0) {
+    if ($i % 3 === 0 && $i % 5 === 0) {
         echo "FizzBuzz <br/>"; 
     }
 }
